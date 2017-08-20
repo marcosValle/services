1360974902daeb073fd77169d8c32b925d06c583
Marcos Valle <marcos.valle01@gmail.com>
2017-08-20 20:01:21 -0300

    DNS, DHCP and HTTP

--- dns/db.gciber.com
+++ dns/db.gciber.com
@@ -1,16 +1,16 @@
-$ORIGIN .
+$ORIGIN gciber.com.
+
 $TTL 604800	; 1 week
-gciber.com		IN SOA	ns.gciber.com. root.gciber.com. (
-				3          ; serial
+@	 IN		 SOA	gciber.com. root.gciber.com. (
+				13         ; serial
 				604800     ; refresh (1 week)
 				86400      ; retry (1 day)
 				2419200    ; expire (4 weeks)
 				604800     ; minimum (1 week)
-				)
-			NS	ns.gciber.com.
-$ORIGIN gciber.com.
-$TTL 300	; 5 minutes
-debian			A	10.0.2.101
-			TXT	"00586958b7e03ff798010083b2b325a066"
-$TTL 604800	; 1 week
-ns			A	10.0.2.1
+				)	
+
+@	IN		NS	ns.gciber.com.
+@	IN		A	10.0.2.103
+ns	IN		A	10.0.2.1
+mvalle	IN		A	10.0.2.103
+
--- http_config.sh
+++ http_config.sh
@@ -1,15 +1,38 @@
+#Reference: https://geekflare.com/apache-web-server-hardening-security/#21-Remove-Server-Version-Banner
 #!/bin/bash
 
 source utils.sh
 
+# do not leak Apache version
 function bannerDisable {
+    #change Header to production only, i.e. Apache
     echo "ServerTokens Prod" >> /etc/apache2/apache2.conf
+    #remove the version information from the page generated like 403, 404, 502, etc
     echo "ServerSignature Off" >> /etc/apache2/apache2.conf
 }
 
+function createVHosts {
+    mkdir -p /var/www/gciber.com/public_html
+    mkdir -p /var/www/mvalle.com/public_html
+
+    cp -p http/gciber_index.html /var/www/gciber.com/public_html/index.html
+    cp -p http/mvalle_index.html /var/www/mvalle.gciber.com/public_html/index.html
+
+    chown -R www-data:www-data /var/www/gciber.com/public_html
+    chown -R www-data:www-data /var/www/mvalle.gciber.com/public_html
+
+    cp http/apache2.conf /etc/apache2/apache2.conf
+    cp -r http/sites-available /etc/apache2
+
+    a2ensite gciber.com.conf mvalle.gciber.com.conf
+    a2dissite 000-default.conf
+
+    systemctl reload apache2.service
+}
+
 checkRoot
-up
-apt install apache2
+#up
+#apt install apache2
 #add mod_security
 #add fail2ban
 bannerDisable
